60fe6283682d0bbdc95b5fd68e2c3f479615b6a5
SGladkis <57638974+SGladkis@users.noreply.github.com>
2023-10-27 04:20:28 +0200

    v1

---
+++ coreset.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class Coreset:
+    def __init__(self, X, k, eps):
+        self.X = X
+        self.k = k
+        self.eps = eps
+
+    def set_k(self, k):
+        self.k = k
+
+    def set_X(self, X):
+        self.X = X
+
+    def _compute_m(self):
+        '''Compute coreset size - the m value'''
+        self.m = np.int64(self.X.shape[1] * self.k * np.log2(self.k) / np.power(self.eps, 2))
+        if self.m > self.X.shape[0] * 0.2:
+            self.m = int(self.k * 0.01 * self.X.shape[0])
+
+    def _compute_coreset(self):
+        '''coreset calculation'''
+        dist = np.power(self.X - self.X.mean(axis=0), 2).sum(axis=1)
+        q = 0.5 / self.X.shape[0] + 0.5 * dist / dist.sum()
+        indices = np.random.choice(self.X.shape[0], size=self.m, replace=True)
+        X_cs = self.X[indices, :]
+        w_cs = 1.0 / (self.m * q[indices])
+        return X_cs, w_cs
+
+    def compute(self):
+        self._compute_m()
+        print("coreset size: ", self.m)
+        return self._compute_coreset()
